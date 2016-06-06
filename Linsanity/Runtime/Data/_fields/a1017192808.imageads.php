1ef980f6349743dd46a1ec7f788b498fe26e971f
Jeffery <heacheuy@live.cn>
2016-06-06 22:27:08 +0800

    Delete a1017192808.imageads.php

--- Linsanity/Runtime/Data/_fields/a1017192808.imageads.php
+++
@@ -1,1 +0,0 @@
-a:8:{i:0;s:2:"id";i:1;s:5:"title";i:2;s:4:"href";i:3;s:9:"imagename";i:4;s:9:"imagepath";i:5;s:3:"tid";s:3:"_pk";s:2:"id";s:5:"_type";a:6:{s:2:"id";s:6:"int(5)";s:5:"title";s:8:"char(20)";s:4:"href";s:9:"char(150)";s:9:"imagename";s:8:"char(40)";s:9:"imagepath";s:8:"char(15)";s:3:"tid";s:10:"tinyint(2)";}}
