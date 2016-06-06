7f5ab2c58abfea33c90eb43f7ce0676b6dbe5020
Jeffery <heacheuy@live.cn>
2016-06-06 22:27:31 +0800

    Delete a1017192808.category.php

--- Linsanity/Runtime/Data/_fields/a1017192808.category.php
+++
@@ -1,1 +0,0 @@
-a:7:{i:0;s:2:"id";i:1;s:5:"title";i:2;s:6:"isshow";i:3;s:6:"corder";i:4;s:6:"idname";s:3:"_pk";s:2:"id";s:5:"_type";a:5:{s:2:"id";s:10:"tinyint(2)";s:5:"title";s:8:"char(20)";s:6:"isshow";s:10:"tinyint(1)";s:6:"corder";s:10:"tinyint(2)";s:6:"idname";s:8:"char(20)";}}
